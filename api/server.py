b5098dc165e1f1d41ef41fc32e2d142e3820cfd6
screazoe <castillorealberto@live.com>
2021-12-10 08:24:19 -0600

    GET y POST del servidor

--- api/server.py
+++ api/server.py
@@ -1,36 +1,61 @@
 from logging import log
-from flask import Flask, request
+from operator import methodcaller
+from flask import Flask, request, jsonify
+from flask.helpers import make_response
 from flask_restful import Resource, Api
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
+from motor import motor
+
+
 import json
 
 from werkzeug.wrappers import Request
 
-app = Flask(__name__) #Creamos el servidor con Flask
-CORS(app) #Permitimos la política CORS
-api = Api(app) #Le damos el caracter de API
 
+app = Flask(__name__) #Creamos el servidor con Flask
+CORS(app, resources={r"/":{"origins":"*"}}) #Permitimos la política CORS
+app.config['CORS_HEADERS']= 'Content-type'
+
+consulta=""
 data="" #Almacena las vista del sitio web (fichas técnicas)
-consulta="" #El string de la consulta del usuario
 orden="" #int {0: Relevancia, 1: Autor , 2:Fecha}
 
-class HelloWorld(Resource):
-    def get(self):
-        return data #Exponemos el archivo con las fichas técnicas
 
-api.add_resource(HelloWorld, '/')
-
-
-@app.route('/consulta', methods=["POST"])
-def obtener_consulta():
+@app.route("/consulta", methods=["POST"])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
+def consultar():
     datos= request.get_json(force=True, cache=True) #Obtenemos el JSON que envía el FrontEnd
-    print("DATOS: ",datos) #Imprime lo que se recibe
-    return 
+    global consulta
+    consulta= datos["consulta"]
+    print(consulta)
+    headers = {'Content-Type': 'text/plain'}
+    return make_response("Consulta hecha", 200, headers)
+
+@app.route("/resultados", methods=["GET"])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
+def algoritmo():
+    data= abrirJSON()
+    return motor(consulta, data)
+
+
+
+
+'''
+class Consulta(Resource):
+    def post(self):
+        datos= request.get_json(force=True, cache=True) #Obtenemos el JSON que envía el FrontEnd
+        consulta= datos["consulta"]
+        print(consulta)
+        headers = {'Content-Type': 'text/plain'}
+        return make_response("Consulta hecha", 200, headers)
+'''
+#api.add_resource(Consulta, '/consulta')
+    
 
 
 
 def abrirJSON():
-    with open('data.json') as file:
+    with open('articulos.json') as file:
         data=json.load(file)
     return data
 
